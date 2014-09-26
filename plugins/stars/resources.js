832ed36af3b468a21b42220b11056b2d853aed63
pmcamacho <pmayocamacho@gmail.com>
2014-09-26 18:08:37 +0100

    changed module used to request stars count from node repo. improved request errors handling

--- plugins/stars/resources.js
+++ plugins/stars/resources.js
@@ -1,41 +1,35 @@
-var config = require('config');
 var Hapi = require('hapi');
+var Request = require('request');
 var Boom = require('boom');
 
-var https = require('https');
+var secret = require('./../../config/secret.json').secret;
 
-exports.getStarsCount = function (request, reply) {
+exports.getStarsCount = function(request, reply) {
 
-  var options = {
-			host: 'api.github.com',
-			port: 443,
-			path: '/repos/joyent/node',
+  	var options = {
+			uri: 'https://api.github.com/repos/joyent/node',
 			method: 'GET',
 			headers : {
-	            'User-Agent' : 'nodejs'
+	            'User-Agent' : secret.useragent,
 	        }
 	};
 
-	var callback = function(req) {
-		var jsonString = '';
-		req.on('data', function(chunk) {
-			jsonString += chunk;
-		});
-
-		req.on('end', function() {
-			//TODO: stargazers_count, watchers and watchers_count fields have the same value on the object returned. Choose the right one.
-			var jsonObject = JSON.parse(jsonString);
-			var starsCount = { stars_count : jsonObject.stargazers_count};
+	var callback = function(error, response, body) {
+		if(!error && response.statusCode == 200) {
+			var jsonObject = JSON.parse(body);
+			var starsCount = { stars_count: jsonObject.stargazers_count };
 			reply(starsCount);
-		});
+		} else if(!error && response.statusCode != 200){
+			reply({
+				statusCode: response.statusCode,
+				message: "Could not retrieve stars count from node repo.",
+				error: JSON.parse(body).message
+			});
+		} else {
+			reply(Boom.badImplementation());
+		}
+	}
 
-	};
-
-	var req =  https.request(options, callback);
-	req.on('error', function(e) {
-		reply(Boom.badImplementation('Could not retrieve stars count from node.js github repo.'));
-	});
-
-	req.end();
+	Request(options, callback);
 
 };
